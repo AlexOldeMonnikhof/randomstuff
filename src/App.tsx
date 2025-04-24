b1851182a4168184bef35bb0eee2b0259ad8fc16
Alex Olde Monnikhof <a.oldemonnikhof@gmail.com>
2025-04-25 01:26:15 +0200

    added mandelbrot component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Mandelbrot from './components/Mandelbrot'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App: React.FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Router>
+      <nav style={{ marginBottom: '1rem' }}>
+        <Link to="/">Homepage</Link> {' - '}
+        <Link to="/mandelbrot">Mandelbrot</Link>
+      </nav>
 
-export default App
+      <Routes>
+        <Route path="/"></Route>
+        <Route path="/mandelbrot" element={<Mandelbrot />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/components/Mandelbrot.tsx
@@ -0,0 +1,9 @@
+const Mandelbrot = () => {
+    return (
+        <div>
+            <h1>test</h1>
+        </div>
+    );
+};
+
+export default Mandelbrot;
